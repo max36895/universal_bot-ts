8208adea7de0b6e304f43b7df9b0357bf800ffbe
max36895 <max18071995@mail.ru>
2023-07-02 12:07:24 +0300

    1.5
    fix console script

--- console/template/index.ts
+++ console/template/index.ts
@@ -7,13 +7,10 @@ import {Bot} from 'umbot';
 import {{name}}Config from './config/{{name}}Config';
 import {{name}}Params from './config/{{name}}Params';
 import {__className__Controller} from './controller/__className__Controller';
-import {IncomingMessage, ServerResponse} from 'http';
 
 const bot = new Bot();
 bot.initTypeInGet();
 bot.initConfig({{name}}Config);
 bot.initParams({{name}}Params);
 bot.initBotController((new __className__Controller()));
-module.exports = async (req: IncomingMessage, res: ServerResponse) => {
-    bot.start(req, res)
-};
+bot.start({{hostname}}, {{port}});
