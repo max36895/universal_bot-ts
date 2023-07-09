f94cf69694e556aac58fc5cda1ca4897490a1318
max36895 <max18071995@mail.ru>
2023-07-09 11:48:25 +0300

    remove game config

--- examples/config/skillGameConfig.ts
+++
@@ -1,9 +0,0 @@
-import {IAppConfig} from '../../src/mmApp';
-
-export default function (): IAppConfig {
-    return {
-        json: __dirname + '/../json',
-        error_log: __dirname + '/../errors',
-        isLocalStorage: true
-    };
-}
